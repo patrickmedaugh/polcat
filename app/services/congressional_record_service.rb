c8b83a71a488ba274b72cf733aba2272507bed15
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-16 08:55:36 -0600

    Connected socrata api

---
+++ app/services/congressional_record_service.rb
@@ -0,0 +1,13 @@
+require 'json'
+require 'soda/client'
+
+class CongressRecordService
+
+  def call_api
+    client = SODA::Client.new({:domain => "opendata.socrata.com/", :app_token => ENV['SODA_KEY']})
+    response = client.get("45vt-g2zi")
+    response
+  end
+end
+crs = CongressRecordService.new
+puts crs.call_api
