f4d031f93f186659059f8a78fb41932d0deca881
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-22 15:45:04 -0600

    Adds testing

---
+++ spec/services/govtrack_service_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe GovtrackService do
+  describe "votes" do
+    it "20 most recent returned for representative" do
+      VCR.use_cassette("vote_results") do
+        votes = GovtrackService.retrieve_recent_votes("400090")
+        expect(votes.first.id).to eq("\n116690")
+      end
+    end
+
+  end
+end
---
+++ spec/services/sunlight_service_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe SunlightService do
+  describe "" do
+    ss = SunlightService.new
+
+    it "can find representatives by state" do
+      VCR.use_cassette("rep_results") do
+        reps = ss.find_by_state("MT")
+        expect(reps['results'].first['bioguide_id']).to eq("Z000018")
+      end
+    end
+
+    it "can find representative by votesmart id" do
+      VCR.use_cassette("rep_results") do
+        rep = ss.find_by_votesmart("345")
+        expect(rep['results'].first['bioguide_id']).to eq("C000984")
+      end
+    end
+
+    it "can retrieve committees for a representative" do
+      VCR.use_cassette("rep_results") do
+        committees = ss.get_committees("C000984")
+        expect(committees['count']).to eq(6)
+      end
+    end
+
+    it "can retrieve floor updates for a representative" do
+      VCR.use_cassette("rep_results") do
+        floor_updates = ss.get_floor_updates("C000984")
+        expect(floor_updates['page']).to eq({'count'=>0, 'per_page'=>20, 'page'=>1})
+      end
+    end
+
+  end
+end
