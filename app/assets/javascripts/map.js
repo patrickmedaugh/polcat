1f1d1fec5077193f3f7c4eeeb515c7272ca9abd4
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-23 15:14:48 -0600

    rep show page styling

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,46 +1,24 @@
-// $(document).ready(function() {
-//   $('#map').usmap({
-//     click: function(event, data) {
-//       $.ajax({
-//         type: "GET",
-//         url: "/states/" + data.name + ".json",
-//         success: function(reps){
-//           $('#representatives').empty();
-//           // $('#representatives').addClass("collection");
-//           $.each(reps.results, function(index, rep){
-//             $('#representatives').append("<a class='collection-item' href= '/representative/" + rep.votesmart_id + "' ><p>" + rep.first_name + " " + rep.last_name + "</p></a>");
-//             // $('#map').css('display', 'none');
-//             })//each
-//             // $('#representatives').append("<a id='mapback' class='collection-item'><p>Back to Map</p></a>");
-//         }//success
-//       })//ajax
-//     }//click
-//
-//   });//map
-//   // $('#mapback').click(function(){
-//   //   $('#map').css('display', 'inline');
-//   // });
-// });//document
-
 $(document).ready(function() {
-    $('#map').usmap({
-      click: function(event, data) {
-        $.ajax({
-          type: "GET",
-          url: "/states/" + data.name + ".json",
-          success: function(reps){
-            console.log(reps.results);
-            $('#representatives').empty();
-            // console.log(JSON.stringify(reps[0]));
-            $.each(reps.results, function(index, rep){
-              console.log(JSON.stringify(rep));
-              $('#representatives').append("<a href= '/representative/" + rep.votesmart_id + "' ><p>" + rep.last_name + "</p></a>");
-            //   $('#map').addClass('.hidden');
-            });
-          }
-        })
-        $('#clicked-state').text('You clicked: '+ data.name);
-    }
+  $('.mapback').click(function(){
+    console.log('mapback pressed');
+    // $('representatives').empty();
+    $('#map').removeClass('hidden');
   });
-
-});
+  $('#map').usmap({
+    click: function(event, data) {
+      $.ajax({
+        type: "GET",
+        url: "/states/" + data.name + ".json",
+        success: function(reps){
+          $('#representatives').empty();
+          $('#representatives').addClass("collection");
+          $.each(reps.results, function(index, rep){
+            $('#representatives').append("<a class='collection-item' href= '/representative/" + rep.votesmart_id + "' ><p>" + rep.first_name + " " + rep.last_name + "</p></a>");
+            $('#map').addClass('hidden');
+          })//each
+          $('#representatives').append("<a class='mapback collection-item'>Back to Map</a>");
+        }//success
+      })//ajax
+    }//click
+  });//map
+});//document
