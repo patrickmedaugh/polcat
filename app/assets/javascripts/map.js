1732dd8a174530f159da90ea3f1f6dddce11e8bd
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-24 10:47:52 -0600

    heroku fix

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,33 +1,28 @@
 $(document).ready(function() {
-    $('#map').usmap({
+  $('.mapback').click(function(){
+    console.log('mapback pressed');
+    // $('representatives').empty();
+    $('#map').removeClass('hidden');
   });
-
-});
-// $(document).ready(function() {
-//   $('.mapback').click(function(){
-//     console.log('mapback pressed');
-//     // $('representatives').empty();
-//     $('#map').removeClass('hidden');
-//   });
-//   $('#map').usmap({
-//     click: function(event, data) {
-//       $.ajax({
-//         type: "GET",
-//         status: 200,
-//         url: "/states/" + data.name + ".json",
-//         success: function(reps){
-//           $('#representatives').empty();
-//           $('#representatives').addClass("collection");
-//           $('#representatives').append("<a class='dropdown-button btn' data-activates='#dropdown1'>" + data.name + " State Representatives</a>");
-//           $('#representatives').append("<ul id='dropdown1' class=''></ul>");
-//           console.log(data);
-//           $.each(reps.results, function(index, rep){
-//             console.log(rep);
-//             // $('#representatives').append("<a class='collection-item' href= '/representative/" + rep.votesmart_id + "' ><p>" + rep.first_name + " " + rep.last_name + "</p></a>");
-//             $('#dropdown1').append("<li><a class='collection-item' href= '/representative/" + rep.bioguide_id + "' >" + rep.first_name + " " + rep.last_name + "</a></li>");
-//           })//each
-//         }//success
-//       })//ajax
-//     }//click
-//   });//map
-// });//document
+  $('#map').usmap({
+    click: function(event, data) {
+      $.ajax({
+        type: "GET",
+        status: 200,
+        url: "/states/" + data.name + ".json",
+        success: function(reps){
+          $('#representatives').empty();
+          $('#representatives').addClass("collection");
+          $('#representatives').append("<a class='dropdown-button btn' data-activates='#dropdown1'>" + data.name + " State Representatives</a>");
+          $('#representatives').append("<ul id='dropdown1' class=''></ul>");
+          console.log(data);
+          $.each(reps.results, function(index, rep){
+            console.log(rep);
+            // $('#representatives').append("<a class='collection-item' href= '/representative/" + rep.votesmart_id + "' ><p>" + rep.first_name + " " + rep.last_name + "</p></a>");
+            $('#dropdown1').append("<li><a class='collection-item' href= '/representative/" + rep.bioguide_id + "' >" + rep.first_name + " " + rep.last_name + "</a></li>");
+          })//each
+        }//success
+      })//ajax
+    }//click
+  });//map
+});//document
