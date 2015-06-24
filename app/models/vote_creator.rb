1ffb157b2fcbded39b35b9588f62bca331085d73
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-24 16:14:32 -0600

    add thread to votecreator and favicon

--- app/models/vote_creator.rb
+++ app/models/vote_creator.rb
@@ -1,6 +1,8 @@
 class VoteCreator
 
   def self.retrieve_recent_votes(govtrack_id)
-    GovtrackService.retrieve_recent_votes(govtrack_id)
+    Thread.new{
+      GovtrackService.retrieve_recent_votes(govtrack_id)
+    }.value
   end
 end
