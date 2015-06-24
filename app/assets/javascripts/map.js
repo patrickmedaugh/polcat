55df408f8cc2a4c445a6c50183805db6c84b6bad
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-24 09:54:01 -0600

    jquery troubleshoot

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -14,11 +14,12 @@ $(document).ready(function() {
           $('#representatives').addClass("collection");
           $('#representatives').append("<a class='dropdown-button btn' data-activates='#dropdown1'>" + data.name + " State Representatives</a>");
           $('#representatives').append("<ul id='dropdown1' class=''></ul>");
-          $.each(reps.results, function(index, rep){
-            console.log(rep);
+          console.log(data);
+          // $.each(reps.results, function(index, rep){
+            // console.log(rep);
             // $('#representatives').append("<a class='collection-item' href= '/representative/" + rep.votesmart_id + "' ><p>" + rep.first_name + " " + rep.last_name + "</p></a>");
-            $('#dropdown1').append("<li><a class='collection-item' href= '/representative/" + rep.bioguide_id + "' >" + rep.first_name + " " + rep.last_name + "</a></li>");
-          })//each
+            // $('#dropdown1').append("<li><a class='collection-item' href= '/representative/" + rep.bioguide_id + "' >" + rep.first_name + " " + rep.last_name + "</a></li>");
+          // })//each
         }//success
       })//ajax
     }//click
