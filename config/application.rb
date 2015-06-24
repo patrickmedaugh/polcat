e225b2d85614c96f1490dc1a8ec2696deb0820cc
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-24 10:33:25 -0600

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
-# howdy
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
