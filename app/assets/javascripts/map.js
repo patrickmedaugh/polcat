084ac03811c9b2048c565b50b795c065af4a3602
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-06-24 10:44:40 -0600

    heroku fix

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,25 +1,5 @@
 $(document).ready(function() {
     $('#map').usmap({
-      click: function(event, data) {
-        $.ajax({
-          type: "GET",
-          url: "/states/" + data.name + ".json",
-          success: function(reps){
-            $('#representatives').empty();
-            $.each(reps, function(index, rep){
-              console.log(JSON.stringify(rep));
-              $('#representatives').append("<a href= '/representative/" + rep.votesmart_id + "' ><p>" + rep.lastname + "</p></a>");
-              $('#map').addClass('.hidden');
-            });
-          }
-        })
-        // $.get('/states/' + data.name).then(function(reps){
-          // reps.empty();
-          // something to this effect \/ \/l
-        //  reps.append("<div>" + reps "</div>")
-        // });
-        $('#clicked-state').text('You clicked: '+ data.name);
-    }
   });
 
 });
